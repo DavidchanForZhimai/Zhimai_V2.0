a5390a2b1d79086f827d2ef890cd7bae43241e45
David <david@DaviddeiMac.local>
2016-09-19 14:25:48 +0800

    认证

---
+++ Lebao/Me(我的)/ViewControllers/certification（认证）/AuthenticationHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AuthenticationHomeViewController.h
+//  Lebao
+//
+//  Created by David on 16/9/19.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface AuthenticationHomeViewController : BaseViewController
+
+@end
